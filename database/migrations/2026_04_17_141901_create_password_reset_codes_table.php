ecfd56496825672c7eb70834d2043762298ef0dc
cyiza22 <cyizahenriette@Hugues-MacBook-Pro.local>
2026-05-06 14:56:01 +0200

    final changes

--- database/migrations/2026_04_17_141901_create_password_reset_codes_table.php
+++ database/migrations/2026_04_17_141901_create_password_reset_codes_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('code')->nullable();
-            $table->timestamp('expires_at');
+            $table->timestamp('expires_at')->nullable();
             $table->timestamps();
         });
     }
